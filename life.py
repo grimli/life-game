46fa9e33f148877a587eafd0a75bc7910c5589ce
grimli <grimli_clex@yahoo.it>
2014-05-15 18:35:47 +0200

    fixed bug in evolve function

--- life.py
+++ life.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-### game of life
-### Author: Edoardo Bompiani
-### Date: 12/05/2014
-### Version: 0.7
-
 import random
 
 def evolve( array ):
 ### esegue lo step di evoluzione del gioco life su una tabella sferica
    rows = len(array)
    columns = len(array[0])
-   array2 = array
-   
+   array2 = [[0 for j in range(rows)] for i in range(columns)]
+
    for i in range( 0, rows-1 ):
       for j in range( 0, columns-1 ):
          totale = array[(i-1)%(rows)][(j-1)%(columns)]+array[(i-1)%(rows)][j%(columns)]+array[(i-1)%(rows)][(j+1)%(columns)]+array[i%(rows)][(j-1)%(columns)]+array[i%(rows)][j%(columns)]+array[i%(rows)][(j+1)%(columns)]+array[(i+1)%(rows)][(j-1)%(columns)]+array[(i+1)%(rows)][j%(columns)]+array[(i+1)%(rows)][(j+1)%(columns)]
@@ -51,7 +46,7 @@ def start_configuration( lenght = 6 ):
    return board
 
 board = [[]]
-board = start_configuration( 40 )
+board = start_configuration( 7 )
       
 for i,v in enumerate(board):
    print(board[i])
@@ -63,13 +58,3 @@ while True:
    board = evolve( board )
    for i,v in enumerate(board):
       print(board[i])
-
-
-### History:
-### 0.7 - utilizzo la funzione modulo per accorpare su un solo caso la varia casistica nel calcolo della generazione successiva
-### 0.6 - fixed evolve calculation
-### 0.5 - aggiungo configurazione iniziale random
-### 0.4 - completed loop for progressing evolution
-### 0.3 - move the loop to function evolve
-### 0.2 - develop basic loop to calculate next generation
-### 0.1 - how to print a List of list?
