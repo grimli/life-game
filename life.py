33242685afd306df5731379f7a10b2ca4f4c5130
grimli <grimli_clex@yahoo.it>
2014-05-30 17:15:09 +0200

    #2: changed do object programming paradigma

--- life.py
+++ life.py
@@ -1,60 +1,61 @@
 #!/usr/bin/python3
 import random
 
-def evolve( array ):
-### esegue lo step di evoluzione del gioco life su una tabella sferica
-   rows = len(array)
-   columns = len(array[0])
-   array2 = [[0 for j in range(rows)] for i in range(columns)]
-
-   for i in range( 0, rows-1 ):
-      for j in range( 0, columns-1 ):
-         totale = array[(i-1)%(rows)][(j-1)%(columns)]+array[(i-1)%(rows)][j%(columns)]+array[(i-1)%(rows)][(j+1)%(columns)]+array[i%(rows)][(j-1)%(columns)]+array[i%(rows)][j%(columns)]+array[i%(rows)][(j+1)%(columns)]+array[(i+1)%(rows)][(j-1)%(columns)]+array[(i+1)%(rows)][j%(columns)]+array[(i+1)%(rows)][(j+1)%(columns)]
-         #print( totale )
-         if array[i][j] == 0:
-            if totale == 3:
-               array2[i][j]=1
-            else:
-               array2[i][j]=0
-         if array[i][j] == 1:
-            if totale <= 2:
-               array2[i][j]=0
-            elif totale <= 4:
-               array2[i][j]=1
-            else:
-               array2[i][j]=0
-
-   return array2
-
-def start_configuration( lenght = 6 ):
-### genera la tabella iniziale quadrata e di dimensione iniziale lenght
-   board = [[]]
-   line = []
-   random.seed()
-
-   for a in range( lenght ):
+class CellularAutoma:
+   def __init__(self, lenght=6):
+   ### genera la tabella iniziale quadrata e di dimensione iniziale lenght
+      self.board = [[]]
       line = []
-      for b in range( lenght ):
-          tmp = random.randint( -1, 1 )
-          if tmp > 0:
-             line.append( 1 )
-          else:
-             line.append( 0 )
-      board.append( line )
-
-   board.remove([])
-   return board
-
-board = [[]]
-board = start_configuration( 7 )
+      random.seed()
       
-for i,v in enumerate(board):
-   print(board[i])
+      for a in range( lenght ):
+         line = []
+         for b in range( lenght ):
+             tmp = random.randint( -1, 1 )
+             if tmp > 0:
+                line.append( 1 )
+             else:
+                line.append( 0 )
+         self.board.append( line )
+            
+      self.board.remove([])
 
-print("\n")
+   def evolve( self ):
+   ### esegue lo step di evoluzione del gioco life su una tabella sferica
+      rows = len(self.board)
+      columns = len(self.board[0])
+      board2 = [[0 for j in range(rows)] for i in range(columns)]
+
+      for i in range( 0, rows-1 ):
+         for j in range( 0, columns-1 ):
+            totale = self.board[(i-1)%(rows)][(j-1)%(columns)]+self.board[(i-1)%(rows)][j%(columns)]+self.board[(i-1)%(rows)][(j+1)%(columns)]+self.board[i%(rows)][(j-1)%(columns)]+self.board[i%(rows)][j%(columns)]+self.board[i%(rows)][(j+1)%(columns)]+self.board[(i+1)%(rows)][(j-1)%(columns)]+self.board[(i+1)%(rows)][j%(columns)]+self.board[(i+1)%(rows)][(j+1)%(columns)]
+            #print( totale )
+            if self.board[i][j] == 0:
+               if totale == 3:
+                  board2[i][j]=1
+               else:
+                  board2[i][j]=0
+            if self.board[i][j] == 1:
+               if totale <= 2:
+                  board2[i][j]=0
+               elif totale <= 4:
+                  board2[i][j]=1
+               else:
+                  board2[i][j]=0
+
+      self.board = board2
+
+   def show( self ):
+      for i,v in enumerate(self.board):
+         print(self.board[i])
+
+
+dim = input( "Inserisci la dimensione della board: ") 
+board = CellularAutoma( int(dim) )
+
+board.show()
 
 while True:
    ok = input()
-   board = evolve( board )
-   for i,v in enumerate(board):
-      print(board[i])
+   board.evolve( )
+   board.show()
